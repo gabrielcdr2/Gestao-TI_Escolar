c19ec4ef0a555d7497a497e9a19f548104d9fb0a
Gabriel Campos <160979410+gabrielcdr2@users.noreply.github.com>
2025-06-28 19:54:54 -0300

    Alterada a estruturação do projeto e tela inicial repaginada.

---
+++ Gestao_chromebooks/Pages/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Gestao_chromebooks.Pages;
+using SkiaSharp;
+using SkiaSharp.Views.Maui;
+using SkiaSharp.Views.Maui.Controls;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
